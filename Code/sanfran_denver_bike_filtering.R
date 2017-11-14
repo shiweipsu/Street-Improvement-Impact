da5123f40cd68d029359fa1eabe26e2f1d90ddf5
jamgreen <jamaal.green@gmail.com>
2017-11-14 14:34:29 -0800

    modified and cleaned up the bike filtering scripts to allow for direct download from the open data website

--- Code/sanfran_denver_bike_filtering.R
+++ Code/sanfran_denver_bike_filtering.R
@@ -1,16 +1,17 @@
 #isolate bicycle networks in Denver and San Francisco in commercial zones
 if(!require("pacman")){install.packages("pacman"); library(pacman)}
-p_load(sf, ggplot2, dplyr)
+p_load(sf, stringr, ggplot2, dplyr)
 
-#this is WGS84
-denver_zoning <-st_read("Data/zoning.shp")
+temp <- tempfile()
+download.file("https://www.denvergov.org/media/gis/DataCatalog/denver_bicycle_facilities/shape/denver_bicycle_facilities.zip",
+              temp)
+denver_bike <- st_read(unzip(temp))
+unlink(temp)
+
+denver_zoning <- st_read("Data/zoning.shp")
+
 denver_commercial <- denver_zoning %>% 
   filter(ZONE_DIST_ == "Commercial Mixed Use")
-
-denver_bike <- st_read(unzip("Data/Bicycle_Network/denver_bicycle_facilities.zip"), 
-                       quiet = TRUE)
-
-#removed the unzipped bicycle network folder
 
 #filter out bike lane, buffered bike lane, cycle track, bus/bike lane
 denver_bike <- denver_bike %>% filter(EXISTING_F == "BL" | EXISTING_F == "BufBL"|
@@ -27,21 +28,21 @@ denver_bike <- denver_bike %>% st_join(denver_commercial, left = FALSE)
 #denver bike commercial corridors
 st_write(denver_bike, "Data/Bicycle_Network/denver_bike_commercialzones.shp", delete_dsn = TRUE)
 
-#san francisco zoning
+#bring in sf data
+sf_bike <- st_read("https://data.sfgov.org/api/geospatial/x3cv-qums?method=export&format=GeoJSON")
 
-sf_bike <- st_read(unzip("Data/Bicycle_Network/SFMTABikewayNetwork.zip"), quiet = TRUE)
-file.remove(list.files(pattern = "geo_export*",recursive = F))
+sf_bike$install_year <- as.numeric(as.character(sf_bike$install_year))
 
-sf_bike <- sf_bike %>% filter(install_ye > 2008)
-sf_bike <- sf_bike %>% filter(innovative == "BIKE BOULEVARD"| innovative == "BUFFERED BIKE LANE" |
-                                innovative == "SEPARATED BIKEWAY")
 
-sf_zoning <- st_read(unzip("Data/San_FranZoningDistricts.zip"), quiet = TRUE)
-file.remove(list.files(pattern = "geo_export*",recursive = F))
+sf_bike <- sf_bike %>% filter(install_year > 2008)
+sf_bike <- sf_bike %>% filter(innovative_treatments == "BIKE BOULEVARD"| 
+                                innovative_treatments == "BUFFERED BIKE LANE" |
+                                innovative_treatments == "SEPARATED BIKEWAY")
 
+sf_zoning <- st_read("https://data.sfgov.org/api/geospatial/8br2-hhp3?method=export&format=GeoJSON")
 sf_zoning <- sf_zoning %>% filter(gen == "Commercial"| gen == "Mixed Use")
 
 sf_bike <- st_join(sf_bike, sf_zoning, left = FALSE)
 st_write(sf_bike, "Data/Bicycle_Network/sf_bike_commercialzones.shp", delete_dsn = TRUE)
 
-rm(denver_bike, denver_commercial, denver_zoning, sf_bike, sf_zoning)
+rm(denver_bike, denver_commercial, denver_zoning, sf_bike, sf_zoning, temp)
