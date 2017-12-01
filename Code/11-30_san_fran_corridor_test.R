8f723187b8e0974e6e1f725c5f726147b1d636e9
jamgreen <jamaal.green@gmail.com>
2017-11-30 16:56:24 -0800

    changed from block groups to blocks and the comparators even out

--- Code/11-30_san_fran_corridor_test.R
+++ Code/11-30_san_fran_corridor_test.R
@@ -15,34 +15,35 @@ sf_compare <- st_transform(sf_compare, 5070)
 sf_treat <- st_transform(sf_treat, 5070)
 
 #grab block group shapes for san francisco and lehd data
-sf_city <- block_groups(state = "CA", county = "San Francisco")
+#sf_city <- block_groups(state = "CA", county = "San Francisco")
+sf_city <- blocks(state = "CA", county = "San Francisco")
 
 years <- 2007:2015
 
 ca_lehd <- map_df(years, grab_lodes, state = "ca", lodes_type = "wac", job_type = "JT01", 
                   segment = "S000", download_dir = "Data/lodes_raw")
 
-ca_lehd <- ca_lehd %>% mutate(geoid = str_sub(w_geocode, 1, 12)) %>% group_by(year, geoid) %>% 
-  summarise_if(is.numeric, sum)
+#ca_lehd <- ca_lehd %>% mutate(geoid = str_sub(w_geocode, 1, 12)) %>% group_by(year, geoid) %>% 
+ # summarise_if(is.numeric, sum)
 
-ca_lehd <- ca_lehd %>%inner_join(sf_city, by =c("geoid" = "GEOID")) %>% st_as_sf()
+ca_lehd <- ca_lehd %>%inner_join(sf_city, by =c("w_geocode" = "GEOID10")) %>% st_as_sf()
 
 ca_lehd <- st_transform(ca_lehd, 5070)
 
 #create buffers and get the spatial join between our buffered polygons and shapes
 #and filter out non-null block groups for the intersection
-sf_compare_buff <- st_buffer(sf_compare, 20)
-sf_treat_buff <- st_buffer(sf_treat, 5)
+sf_compare_buff <- st_buffer(sf_compare, 10)
+sf_treat_buff <- st_buffer(sf_treat, 10)
 
 ca_lehd <- st_join(ca_lehd, sf_compare_buff)
 ca_lehd <- st_join(ca_lehd, sf_treat_buff)
 
-ca_lehd <- ca_lehd %>% select(-30:-64)
+ca_lehd <- ca_lehd %>% select(-29:-53, -55:-71)
 
 ca_lehd_corridors <- ca_lehd %>% filter(!is.na(StName) | !is.na(full_stree))
 ca_lehd_corridors <- ca_lehd_corridors %>% mutate(Treatment = ifelse(!is.na(full_stree), 1, 0))
 
-ca_lehd_corridors <- ca_lehd_corridors %>% select(geoid, year, TotEmp = C000, 
+ca_lehd_corridors <- ca_lehd_corridors %>% select(GEOID = w_geocode, year, TotEmp = C000, 
                                                   Retail = CNS07, ArtsRec = CNS17, 
                                                   Accomodation = CNS18, StName, Comparator,
                                                   TreatStreet = full_stree, Installed = install_ye,
