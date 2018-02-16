7d579439ecf3966f5f27fb7f5df28b9ab81ce5de
jamgreen <jamaal.green@gmail.com>
2018-02-16 15:27:14 -0800

    remove st_intersection and keep only the spatial join

--- Code/spatial_join_example.R
+++ Code/spatial_join_example.R
@@ -5,6 +5,4 @@ p_load(sf, dplyr)
 indy_buff <- st_read("Data/indy_corridor_buffer.shp")
 indy_lehd <- st_read("Data/indy_lehd_NAD83.shp")
 
-corridor_lehd <- st_intersection(indy_buff, indy_lehd)
-
 corridor_poly <- st_join(indy_lehd, indy_buff, left = FALSE)
