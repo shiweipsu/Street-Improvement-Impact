9a9922b168168c3e555447c14f97305d0807dab6
jamgreen <jamaal.green@gmail.com>
2018-02-20 14:48:13 -0800

    modified the indianapolis lehd file

---
+++ Code/indianapolis_grab_lehd.R
@@ -0,0 +1,12 @@
+source("Code/grab_place_lehd_func.R")
+
+years <- 2004:2015
+target_state <- "in"
+target_county <- "marion"
+target_place <- "Indianapolis city (balance)"
+
+indy_lehd <- nitc_lehd(years = years, target_state = target_state, target_county = target_county, 
+                       target_place = target_place)
+st_write(indy_lehd, "Data/indy_lehd.geojson", delete_dsn = TRUE)
+
+
