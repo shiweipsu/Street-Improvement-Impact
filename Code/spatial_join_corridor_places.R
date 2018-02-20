b75feaf510d5d62c7f8ffd6a988de34e6de04e6a
jamgreen <jamaal.green@gmail.com>
2018-02-20 11:26:12 -0800

    modified the spatial join file

--- Code/spatial_join_corridor_places.R
+++ Code/spatial_join_corridor_places.R
@@ -5,4 +5,8 @@ p_load(sf, dplyr)
 indy_buff <- st_read("Data/indy_corridor_buffer.shp")
 indy_lehd <- st_read("Data/indy_lehd_NAD83.shp")
 
+buff_crs <- st_crs(indy_buff)
+indy_lehd <- st_transform(indy_lehd, crs = buff_crs)
+
 corridor_poly <- st_join(indy_lehd, indy_buff, left = FALSE)
+st_write(corridor_poly, "Data/indy_corridor_lehd_NAD83.shp", delete_dsn = TRUE)
