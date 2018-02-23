a595b849f113254668340e473fd74021dace5439
jamgreen <jamaal.green@gmail.com>
2018-02-23 09:42:08 -0800

    fixed some buggier aspects of the corridor comparison

--- Code/corridor_comparison_functions.R
+++ Code/corridor_comparison_functions.R
@@ -9,8 +9,11 @@ employ_ratio_test <- function(corridor_df) {
 corridor_df <- as.data.frame(corridor_df)
 
 corridor_t <- corridor_df %>% mutate(Business = CNS07 + CNS18,
-                                     Service = CNS07 + CNS16 + CNS17 + CNS18,
-                                     ratio1 = Business/Service)
+                                     Service1 = CNS07 + CNS12 + CNS14 + CNS15 + CNS16 + 
+                                       CNS17 + CNS18 + CNS19,
+                                     Service2 = CNS07 + CNS12 + CNS14 + CNS17 + CNS18 + CNS19,
+                                     ratio1 = Business/Service1,
+                                     ratio2 = Business/Service2)
 return(corridor_t)  
 }
 
@@ -23,11 +26,15 @@ corridor_grouped_df <- corridor_df %>%  group_by(Name, year) %>%
   summarise_if(is.numeric, sum) %>% 
   ungroup() %>% 
   mutate(Business = CNS07 + CNS18,
-         Service = CNS07 + CNS16 + CNS17 + CNS18,
-         ratio1 = Business/Service) %>% 
+         Service1 = CNS07 + CNS12 + CNS14 + CNS15 + CNS16 + 
+           CNS17 + CNS18 + CNS19,
+         Service2 = CNS07 + CNS12 + CNS14 + CNS17 + CNS18 + CNS19,
+         ratio1 = Business/Service1,
+         ratio2 = Business/Service2) %>% 
   ungroup() %>% group_by(Name) %>% 
   mutate(biz_growth = Business/lag(Business),
-         service_growth = Service/lag(Service))
+         service_growth1 = Service1/lag(Service1),
+         service_growth2 = Service2/lag(Service2))
 
 return(corridor_grouped_df)
 
