a8e5c72e072bebb6e0be6f7ff64c6e782adf23fa
shiweipsu <shi.wei8805@gmail.com>
2018-02-22 17:48:19 -0800

    Define service sectors and add up other sectors, pull out Retail, Food_Accom seperately

--- Code/corridor_comparison_functions.R
+++ Code/corridor_comparison_functions.R
@@ -8,12 +8,15 @@ employ_ratio_test <- function(corridor_df) {
   
 corridor_df <- as.data.frame(corridor_df)
 
-corridor_t <- corridor_df %>% mutate(Business = CNS07 + CNS18,
-                                     Service = CNS07 + CNS16 + CNS17 + CNS18,
+corridor_t <- corridor_df %>% mutate(Retail=CNS07,
+                                     Food_Accom=CNS08,
+                                     Business = CNS07 + CNS18,
+                                     Service = CNS07 +  CNS12 + CNS14 + CNS15 + CNS16 + CNS17 + CNS18 + CNS19,
                                      ratio1 = Business/Service)
 return(corridor_t)  
 }
 
+# Definition of Service is from EPA Smart Location Database, 5-tier employment classification scheme, adding retail, service, and entertainment
 
 growth_rate <- function(corridor_df) {
   
@@ -22,7 +25,9 @@ corridor_df <- as.data.frame(corridor_df)
 corridor_grouped_df <- corridor_df %>%  group_by(Name, year) %>% 
   summarise_if(is.numeric, sum) %>% 
   ungroup() %>% 
-  mutate(Business = CNS07 + CNS18,
+  mutate(Retail=CNS07,
+         Food_Accom=CNS18,
+         Business = CNS07 + CNS18,
          Service = CNS07 + CNS16 + CNS17 + CNS18,
          ratio1 = Business/Service) %>% 
   ungroup() %>% group_by(Name) %>% 
