0ab7cdebdaea6760a08ffbc589181b377b37a49b
jamgreen <jamaal.green@gmail.com>
2018-03-05 09:10:37 -0800

    changed buffer size in spatjoin and converted minneapolis to geojson because of irregular geometries

--- Code/spatial_join_corridor_places.R
+++ Code/spatial_join_corridor_places.R
@@ -1,6 +1,7 @@
 if(!require(pacman)){install.packages("pacman"); library(pacman)}
 p_load(sf, dplyr)
 
+#indianapolis 
 
 indy_buff <- st_read("Data/indianapolis/indy_corridors.shp")
 indy_lehd <- st_read("Data/indianapolis/indy_lehd.geojson")
@@ -11,7 +12,7 @@ buff_crs <- 5070
 indy_buff <- st_transform(indy_buff, crs = buff_crs)
 indy_lehd <- st_transform(indy_lehd, crs = buff_crs)
 
-indy_buff <- st_buffer(indy_buff, dist = 10)
+indy_buff <- st_buffer(indy_buff, dist = 15)
 
 corridor_poly <- st_join(indy_lehd, indy_buff, left = FALSE)
 corridor_poly <- corridor_poly %>% select(3:30, 55:63)
@@ -23,16 +24,18 @@ st_write(corridor_poly, "Data/indianapolis/indy_corridor_lehd_NAD83.shp", delete
 minn_buff <- st_read("Data/minneapolis/minneapolis_corridorsNAD83.shp")
 minn_lehd <- st_read("Data/minneapolis/minn_lehd.geojson")
 
-
-buff_crs <- st_crs(minn_buff)
+buff_crs <- 5070
 
 minn_lehd <- st_transform(minn_lehd, crs = buff_crs)
+minn_buff <- st_transform(minn_buff, crs = buff_crs)
 
-minn_buff <- st_buffer(minn_buff, dist = 10)
+minn_buff <- st_buffer(minn_buff, dist = 15)
+
+minn_lehd <- st_simplify(minn_lehd)
 
 corridor_poly <- st_join(minn_lehd, minn_buff, left = FALSE)
 corridor_poly <- corridor_poly %>% select(3:30, 55:63)
-st_write(corridor_poly, "Data/minneapolis/minn_corridor_lehd_NAD83.shp", delete_dsn = TRUE)
+st_write(corridor_poly, "Data/minneapolis/minn_corridor_lehd_wgs84.geojson", delete_dsn = TRUE)
 
 #seattle 
 
@@ -45,7 +48,7 @@ seattle_buff <- st_zm(seattle_buff)
 
 seattle_lehd <- st_transform(seattle_lehd, crs = buff_crs)
 
-seattle_buff <- st_buffer(seattle_buff, dist = 10)
+seattle_buff <- st_buffer(seattle_buff, dist = 15)
 
 corridor_poly <- st_join(seattle_lehd, seattle_buff, left = FALSE)
 corridor_poly <- corridor_poly %>% select(3:30, 55:63)
