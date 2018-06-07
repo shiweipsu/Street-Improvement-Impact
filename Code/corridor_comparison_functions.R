a374efbcaeebe204e0109daf36030109efffbe7e
Jamaal Green <jamgreen@psu.ds.pdx.edu>
2018-06-07 13:17:29 -0700

    first step in corridor comparison function revision

--- Code/corridor_comparison_functions.R
+++ Code/corridor_comparison_functions.R
@@ -56,8 +56,6 @@ return(corridor_grouped_df)
 
 
 
-
-
 #QCEW Corridor Comparisons-------
 
 
@@ -113,10 +111,6 @@ agg_trend_plot <- function(df_plot, industry, corridor_name,
     geom_line()  +
     geom_rect(aes(xmin = as.Date(construct_date, "%Y"), xmax = as.Date(end_date, "%Y"), ymin = -Inf, ymax = Inf),
               fill = "#adff2f",linetype=0,alpha = 0.03) +
-    # geom_rect(aes(xmin = as.Date(end_date, "%Y") + 1, xmax = as.Date("2015-01-01", "%Y"), ymin = -Inf, ymax = Inf),
-    #           fill = "#7cfc00", linetype=0,alpha = 0.03) +
-    # geom_text(x= construct_date, y = 150,label="Construction",colour="grey40",size = 4, hjust = 0) +
-    # geom_text(x= end_date, y= 150,label="Post \n construction",colour="grey40",size = 4, hjust = 1) +
     geom_point(size = 3, fill="white") +
     scale_shape_manual(values=c(22,21,21,23))+
     scale_x_date(date_breaks = "3 years", date_labels = "%Y") +
