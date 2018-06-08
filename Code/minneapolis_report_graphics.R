9f8e7135c7f6ea2d854cc9808affa82c9054de41
Jamaal Green <jamgreen@psu.ds.pdx.edu>
2018-06-08 15:25:02 -0700

    got final images made with the city trend

--- Code/minneapolis_report_graphics.R
+++ Code/minneapolis_report_graphics.R
@@ -1,64 +1,57 @@
-minn_agg <- minn_lehd %>% 
-group_by(year) %>% summarise_if(is.numeric, sum)  
+if(!require(pacman)){install.packages("pacman"); library(pacman)}
+p_load(here,sf, ggplot2, readr, ggthemes, cowplot, dplyr)
 
-minn_base <- minn_lehd %>% filter(year == 2011) %>% 
-  select(CNS07sum = CNS07, CNS18sum = CNS18) %>% summarise_if(is.numeric, sum)
+source(here::here("Code/corridor_comparison_functions.R"))
 
 
-minn_agg <- cbind(minn_agg, minn_base)
+minn_agg <- read_csv("Data/minneapolis_city_index.csv", 
+                            col_types =  cols(Type = "c", 
+                                             year = "c",
+                                              .default = "n"))
 
-minn_agg <- minn_agg %>% 
-  mutate(retail_idx = CNS07/CNS07sum*100,
-         accom_idx = CNS18/CNS18sum*100) %>% 
-  select(year, CNS07, CNS18, CNS07sum, CNS18sum, retail_idx, accom_idx)
+minn_corridor <- st_read(here::here("Data/minneapolis/minn_corridor_lehd_wgs84.geojson")) %>% 
+  as.data.frame()
 
-minn_agg <- minn_agg %>% filter(!is.na(year))
-
-minn_agg$Type <- "city"
-
-minn_agg <- minn_agg %>% select(Type, year, CNS07_sd = retail_idx, 
-                                CNS18_sd= accom_idx)
-
-readr::write_csv(minn_agg, "Data/minneapolis_city_index.csv")
-
-riverside_agg <- agg_index_trend_table(minn_corridor, group = 2, 2011) %>% 
+lyndale_agg <- agg_index_trend_table(minn_corridor, group = 4, 2008) %>% 
   filter(!is.na(year))
 
-riverside_agg$business_sd <- NULL
+lyndale_agg$business_sd <- NULL
 
-riverside_agg <- bind_rows(riverside_agg, minn_agg)
+lyndale_agg <- bind_rows(lyndale_agg, minn_agg)
 
 #manually build plot----------------------------
 
-riverside_agg$Type <- factor(riverside_agg$Type, 
+lyndale_agg$Type <- factor(lyndale_agg$Type, 
                              levels = c("improvement", "control", "city"))
 
- retail <- ggplot(riverside_agg,aes(x=as.numeric(as.character(year)), 
+ retail <- ggplot(lyndale_agg,aes(x=as.numeric(as.character(year)), 
                                                                  y=CNS07_sd,group=Type,colour=Type,shape=Type))+
-  geom_rect(aes(xmin = 2011, xmax = 2012, ymin = -Inf, ymax = Inf),fill = "darkolivegreen1",linetype=0,alpha = 0.03) +
+  geom_rect(aes(xmin = 2008, xmax = 2009, ymin = -Inf, ymax = Inf),fill = "darkolivegreen1",linetype=0,alpha = 0.03) +
   geom_line() + 
   geom_point(size=3, fill="white") +
   scale_shape_manual(values=c(22,21, 23))+
   scale_x_discrete(breaks=c(2004,2006,2008,2010,2012,2014,2016)) +
   theme_minimal() +
-  labs(title = "Retail Employment:\n Franklin Ave", x="Year",
+  labs(title = "Retail Employment:\nLyndale Ave.", x="Year",
        y="Employment Index",
        caption = "Employment is indexed to 2011 \n Shaded Area is Construction Period")
 
 
 
  
- accom <- ggplot(riverside_agg,aes(x=as.numeric(as.character(year)), 
+ accom <- ggplot(lyndale_agg,aes(x=as.numeric(as.character(year)), 
                           y=CNS18_sd,group=Type,colour=Type,shape=Type))+
-   geom_rect(aes(xmin = 2011, xmax = 2012, ymin = -Inf, ymax = Inf),fill = "darkolivegreen1",linetype=0,alpha = 0.03) +
+   geom_rect(aes(xmin = 2008, xmax = 2009, ymin = -Inf, ymax = Inf),fill = "darkolivegreen1",linetype=0,alpha = 0.03) +
    geom_line() + 
    geom_point(size=3, fill="white") +
    scale_shape_manual(values=c(22,21, 23))+
    scale_x_discrete(breaks=c(2004,2006,2008,2010,2012,2014,2016)) +
    theme_minimal() +
-   labs(title = "Food & Accommodations Employment:\n Franklin Ave", x="Year",
+   labs(title = "Food & Accommodations Employment:\nLyndale Ave.", x="Year",
         y="Employment Index",
         caption = "Employment is indexed to 2011 \n Shaded Area is Construction Period")
  
 
- city_franklin <- plot_grid(retail, accom, align = "h")
+ city_lyndale <- plot_grid(retail, accom, align = "h")
+ 
+ 
