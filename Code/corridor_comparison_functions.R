093ef53622b8609fad40e557edb7093110bcd7f5
shiweipsu <shi.wei8805@gmail.com>
2018-08-31 18:07:44 -0700

    add function dist_trend_plot for distributional analysis

--- Code/corridor_comparison_functions.R
+++ Code/corridor_comparison_functions.R
@@ -439,3 +439,48 @@ city_agg_trend_plot <- function(df_plot, industry, corridor_name,
 }
 
 
+# plot for distributional analysis
+
+
+dist_trend_plot <- function(df_plot, demo, corridor_name, 
+                           demo_code = c("low_inc_perc", "white_perc", "bach_col_perc"), 
+                           construct_year, end_year) {
+  
+  df_plot$Type <- factor(df_plot$Type, levels = rev(levels(df_plot$Type)))
+  
+  #convert year to proper date
+  
+  df_plot$year <-as.character(paste0(df_plot$year, "-01-01"))
+  df_plot$year <- as.Date(df_plot$year, "%Y-%m-%d")
+  
+  #df_plot$year <- year(df_plot$year)
+  
+  #prepare construct_year and end_year as a date
+  
+  construct_date <- as.character(paste0(construct_year, "-01-01"))
+  construct_date <- as.Date(construct_date, "%Y-%m-%d")
+  
+  #construct_date <- year(construct_year)
+  
+  end_date <- as.character(paste0(end_year, "-01-01"))
+  end_date <- as.Date(end_date, "%Y-%m-%d")
+  
+  #end_date <- year(end_year)
+  
+  #making the plot
+  
+  ats_df <- ggplot(df_plot, aes(x = year, y = get(demo_code), shape = Type, group = Type, colour = Type)) + 
+    geom_line()  +
+    geom_rect(aes(xmin = as.Date(construct_date, "%Y"), xmax = as.Date(end_date, "%Y"), ymin = -Inf, ymax = Inf),
+              fill = "#adff2f",linetype=0,alpha = 0.03) +
+    geom_point(size = 3, fill="white") +
+    scale_shape_manual(values=c(22,21,21,23))+
+    scale_x_date(date_breaks = "3 years", date_labels = "%Y") +
+    theme_minimal() +
+    labs(title = glue("Percentage of {demo} Employment:\n {corridor_name}"), x="Year",y="Percentage",
+         caption = "Shaded area represents the construction period") +
+    guides(title = "Street Type")
+  
+  
+  return(ats_df)
+}
