6bb207eb3ba3bc1ebc9c5c4214400c1f7de73f4e
shiweipsu <shi.wei8805@gmail.com>
2018-10-03 09:57:48 -0700

    add function "nitc_rac" to grab rac data

--- Code/grab_place_lehd_func.R
+++ Code/grab_place_lehd_func.R
@@ -39,3 +39,34 @@ nitc_lehd <- function(years, target_state, target_county, target_place) {
 
 
 
+nitc_rac <- function(years, target_state, target_county, target_place) {
+  
+  state_lehd <- grab_lodes(state = target_state, year = years, lodes_type = "rac", 
+                           job_type = "JT01", segment = "S000",
+                           agg_geo = "block",
+                           download_dir = glue::glue("Data/{target_state}_lehd"))
+  
+  place.sf <- places(state = target_state)
+  place.sf <- place.sf %>% filter(NAME == target_place)
+  
+  state_blocks <- if(!is.null(target_county)){
+    state_blocks <- blocks(state = target_state, county = target_county)
+  } else {
+    state_blocks <- blocks(state = target_state)
+  }
+  
+  #place_blocks <- st_intersection(place.sf, state_blocks)
+  place_blocks <- st_join(state_blocks, place.sf, left = FALSE)
+  
+  
+  place_lehd <- place_blocks %>% left_join(state_lehd, 
+                                           by = c("GEOID10" = "h_geocode"))
+  
+  place_lehd <- place_lehd %>% select(4:5, 21, 34:74, 76:78)
+  
+  return(place_lehd)
+  
+  st_write(place_lehd, glue::glue("Data/{target_place}_rac.shp"), delete_dsn = TRUE)
+  
+  
+}
