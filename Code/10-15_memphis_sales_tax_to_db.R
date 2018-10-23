ff93aee7382fd1dcc558bac6100554efbbf0081f
Wei Shi <shi.wei8805@gmail.com>
2018-10-22 17:21:53 -0700

    add S Cooper to both corridors

--- Code/10-15_memphis_sales_tax_to_db.R
+++ Code/10-15_memphis_sales_tax_to_db.R
@@ -2,7 +2,7 @@
 if(!require(pacman)){install.packages("pacman"); library(pacman)}
 p_load(RPostgreSQL, dplyr, dbplyr, readr, tidyverse)
 
-user <- "jamgreen"
+user <- "shiwei"
 host <- "pgsql102.rc.pdx.edu"
 pw <- scan(here::here("batteries.pgpss"), what = "")
 dbname <- "bike_lanes"
@@ -12,7 +12,7 @@ con <- dbConnect("PostgreSQL", host = host, user = user, dbname = dbname,
 
 #i downoaded from google drive, just bring from downloads folder
 
-memphis_sales <- read_csv("Data/corridor_sales_receipts_final.csv")
+memphis_sales <- read_csv("Data/memphis/corridor_sales_receipts_final.csv")
 
 #need to rationalize the table...split the corr_type column, set labels, and street groups
 #split corr_type column
@@ -22,14 +22,15 @@ memphis_sales <- memphis_sales %>%
 
 #Set street group by street_name
 
-#set two groups, filter out S Cooper St ,drop the street_name variable 
+#set two groups, filter out S Cooper St to make it control for both,drop the street_name variable 
 
 memphis_sales <- memphis_sales %>% 
-  mutate(street_group = case_when(district_name == "Madison Ave" | street_name == "Madison" ~ 1,
+  mutate(street_group = case_when(district_name == "Madison Ave" | street_name == "Madison" | district_name == "S Cooper St" ~ 1,
                                   district_name == "Broad Ave" | street_name == "Broad" ~ 2))
 
-memphis_sales <-memphis_sales %>% 
-  filter(district_name != "S Cooper St")
+cooper <- memphis_sales %>% filter(district_name == "S Cooper St") %>% mutate(street_group =2)
+
+memphis_sales <-rbind(memphis_sales, cooper)
 
 memphis_sales <- memphis_sales %>% 
   select(-street_name, -index)
