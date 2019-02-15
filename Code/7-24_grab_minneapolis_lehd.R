03d10ba38893f50ba767fa3fc4b31a75915f92ff
Wei Shi <shi.wei8805@gmail.com>
2019-02-15 15:46:48 -0800

    grab minn rac data

--- Code/7-24_grab_minneapolis_lehd.R
+++ Code/7-24_grab_minneapolis_lehd.R
@@ -1,12 +1,12 @@
 #grab minneapolis lehd and upload to spatial db
 
 if(!require(pacman)){install.packages("pacman"); library(pacman)}
-p_load(here, RPostgreSQL, sf, tidyr,dplyr, dbplyr)
+p_load(here, RPostgreSQL, sf, tidyr,dplyr, dbplyr, rlang)
 
 source(here::here("Code/grab_place_lehd_func.R"))
 
 #query the corridors from bike_lanes
-user <- "jamgreen"
+user <- "shiwei"
 host <- "pgsql102.rc.pdx.edu"
 pw <- scan(here::here("batteries.pgpss"), what = "")
 dbname <- "bike_lanes"
@@ -25,5 +25,18 @@ names(minn_lehd) <- tolower(names(minn_lehd))
 minn_lehd <- minn_lehd %>% filter(!is.na(state))
 
 st_write(minn_lehd, dsn = con, overwrite = TRUE, "minneapolis_lehd", geom_column = "geometry")
+
+
+# grab rac
+minn_rac <- nitc_rac(years = years, target_state = "MN", target_county = "Hennepin", 
+                   target_place = "Minneapolis")
+
+minn_rac$BLOCKCE10 <- NULL
+
+names(minn_rac) <- tolower(names(minn_rac))
+
+minn_rac <- minn_rac %>% filter(!is.na(state)) %>% st_transform(5070)
+
+st_write(minn_rac, dsn = con, overwrite = TRUE, "minn_rac",geom_col="geometry")
 
 dbDisconnect(con)
